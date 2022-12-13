53f016cd2caa85e7653d6e963b032feff687ac6c
rectilineo <alumno.710283@ies-azarquiel.es>
2022-12-13 12:26:58 +0100

    hola gente

--- Program.cs
+++ Program.cs
@@ -1,4 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Dime tu nombre: ");  
-string nombre = Console.ReadLine();
-Console.WriteLine($"Tu nombre es: {nombre}");
+﻿Console.WriteLine ("Buenas tienes que adivinar un numero entre el 1 y el 15, ¿Cuál eliges?");
+var generadorAleatorio = new Random();
+int numero= generadorAleatorio.Next(1,15);
+string numeroadivinado=Console.ReadLine();
+int numeroadivinado1= Int32.Parse(numeroadivinado);
+int vida = 3;
+
+if (numeroadivinado1 == numero) {
+    for (int i=1; i < 2; i++){
+    Console.WriteLine("¡Felicidades, has ganado!");
+    if (i==2);
+    break;
+}
+}
+for (int bucle=0; bucle <3; bucle++){
+if(numeroadivinado1 > numero){
+int vidaRestante = (vida -1);
+Console.WriteLine ($"Lo siento, te quedan {vidaRestante} vidas");
+Console.WriteLine("Tu numero era mayor al que yo pensaba");
+}
+if (numeroadivinado1 < numero){
+    int vidaRestante = (vida-1);
+Console.WriteLine ($"Lo siento, te quedan {vidaRestante} vidas");
+Console.WriteLine("Tu numero es menor al que yo pensaba");
+}
+if (numeroadivinado1==numero) {
+break;
+}
+}
